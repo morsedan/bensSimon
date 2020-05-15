9ac52e70772cb4afa2824913f35843a36c27c700
Dan Morse <djmthedev@gmail.com>
2020-05-14 19:48:08 -0700

    Initial Commit

---
+++ bensSimon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bensSimon
+//
+//  Created by morse on 5/14/20.
+//  Copyright © 2020 morse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
